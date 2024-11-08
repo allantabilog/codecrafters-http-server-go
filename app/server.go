1ed976c31082e35d3829b3e6e8e6a7d505234afc
Allan Tabilog <allan.tabilog@pexa.com.au>
2024-11-08 15:38:15 +1100

    Add comment

--- app/server.go
+++ app/server.go
@@ -11,6 +11,7 @@ import (
 var _ = net.Listen
 var _ = os.Exit
 
+// refactoring required
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
