1d1f5479689b1ddb17ce7dc6352eb76924123b97
allantabilog <allan.tabilog@gmail.com>
2024-11-22 15:22:29 +1100

    Implement a request parser

--- app/server.go
+++ app/server.go
@@ -7,92 +7,81 @@ import (
 	"strings"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
+// temp code to ensure gofmt doesn't remove the imports while unreferenced
 var _ = net.Listen
 var _ = os.Exit
 
-// refactoring required
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+const port = 4221
 
-	// Uncomment this block to pass the first stage
-	//
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+func main() {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
+		fmt.Printf("Failed to bind to port %v\n", port)
 		os.Exit(1)
 	}
 
+	fmt.Printf("Listening on port %v\n", port)
+
 	for {
-		var conn net.Conn
-		conn, err = l.Accept()
+		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
+			fmt.Println("Failed to accept connection")
 			os.Exit(1)
 		}
-		go handleConnection(conn)
+
+		go handleRequest(conn)
 	}
 }
-func prepareEchoResponse(message string) string {
-	return "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + fmt.Sprint(len(message)) + "\r\n\r\n" + message
-}
 
-func handleConnection(conn net.Conn) {
+func handleRequest(conn net.Conn) {
 	defer conn.Close()
 
-	//create a byte array buffer to read the incoming data
 	buf := make([]byte, 4096)
-	// read into the buffer
-	_, err := conn.Read(buf)
+	n, err := conn.Read(buf)
 	if err != nil {
-		fmt.Println("Error reading from connection:", err.Error())
-	}
-
-	// turn the byte array into a string
-	message := string(buf)
-	fmt.Println("Message received: ", message)
-	// split the string into parts delimited by a " "
-	parts := strings.Split(message, " ")
-	// the path is the second part of the message
-	path := parts[1]
-
-	var response string
-	if path == "/" {
-		response = "HTTP/1.1 200 OK\r\n\r\n"
-		_, err = conn.Write([]byte(response))
-		if err != nil {
-			fmt.Println("Error writing to connection:", err.Error())
-			os.Exit(1)
-		}
+		fmt.Println("Failed to read data from connection")
 		return
 	}
 
-	if strings.HasPrefix(path, "/echo") {
-		payloadParts := strings.Split(path, "/")
-		// check that there are two parts to the payload
-		// "echo" and  the message
-		fmt.Printf("Payload parts %v length %v\n: ", payloadParts, len(payloadParts))
-		if len(payloadParts) != 3 {
-			response = "HTTP/1.1 400 Bad Request\r\n\r\n"
-			_, err = conn.Write([]byte(response))
-			if err != nil {
-				fmt.Println("Error writing to connection:", err.Error())
-				os.Exit(1)
-			}
-			return
-		}
-		fmt.Println("Payload: ", payloadParts[2])
-		response = prepareEchoResponse(payloadParts[2])
-	} else {
-		response = "HTTP/1.1 404 Not Found\r\n\r\n"
+	request := string(buf[:n])
+
+	requestParser := RequestParserImpl{}
+	requestLine, err := requestParser.parseRequestLine(request)
+
+	if err != nil {
+		fmt.Println("Invalid request")
+		conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
+		return
 	}
 
-	// write the response back to the client
-	_, err = conn.Write([]byte(response))
-	if err != nil {
-		fmt.Println("Error writing to connection:", err.Error())
-		os.Exit(1)
+	fmt.Printf("verb: %v\n", requestLine.Verb)
+	fmt.Printf("path: %v\n", requestLine.Path)
+	fmt.Printf("version: %v\n", requestLine.Version)
+
+	switch {
+	case requestLine.Path == "/":
+		handleRootRequest(requestLine.Path, conn)
+	case strings.HasPrefix(requestLine.Path, "/echo"):
+		handleEchoRequest(requestLine.Path, conn)
+	case strings.HasPrefix(requestLine.Path, "/user-agent"):
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	default:
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
-	// just adding a comment to trigger the process
+}
+
+func handleRootRequest(path string, conn net.Conn) {
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+}
+
+func handleEchoRequest(path string, conn net.Conn) {
+	fmt.Printf("handling echo request for path: %v\n", path)
+	// extract the message from the path
+	message := strings.Split(path, "/")[2]
+	fmt.Printf("The message is: %v\n", message)
+
+	// construct the response
+	response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)
+
+	conn.Write([]byte(response))
 }
